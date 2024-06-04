5ac65ad1bd7570a7dd2248c83059fbb2baff37c3
majid111 <abdullahalmajid81@gmail.com>
2024-06-05 04:17:24 +0600

    added country based tourists spot search

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// const uri = 'mongodb://localhost:27017';
 // const uri = "mongodb+srv://<username>:<password>@cluster0.bi44cht.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.bi44cht.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -27,10 +28,98 @@ async function run() {
     //should comment below line
     await client.connect();
 
-    
+    const happyToursCollection = client
+      .db("happyToursDB")
+      .collection("happyTours");
+    const countryCollection = client.db("happyToursDB").collection("Countries");
+
+    app.post("/happyTours", async (req, res) => {
+      const newCoffee = req.body;
+      const result = await happyToursCollection.insertOne(newCoffee);
+      res.send(result);
+    });
+
+    app.get("/happyTours", async (req, res) => {
+      const cursor = happyToursCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/happyTours/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await happyToursCollection.findOne(query);
+      res.send(result);
+    });
+
+    //update by id
+    app.put("/happyTours/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+
+      const updatedHappyTours = req.body;
+
+      const happyTours = {
+        $set: {
+          photoUrl: updatedHappyTours.photoUrl,
+          touristsSpotName: updatedHappyTours.touristsSpotName,
+          countryName: updatedHappyTours.countryName,
+          location: updatedHappyTours.location,
+          shortDescription: updatedHappyTours.shortDescription,
+          averageCost: updatedHappyTours.averageCost,
+          seasonality: updatedHappyTours.seasonality,
+          travelTime: updatedHappyTours.travelTime,
+          totaVisitorsPerYear: updatedHappyTours.totaVisitorsPerYear,
+          email: updatedHappyTours.email,
+          userName: updatedHappyTours.userName,
+        },
+      };
+
+      const result = await happyToursCollection.updateOne(
+        filter,
+        happyTours,
+        options
+      );
+      res.send(result);
+    });
+
+    app.delete("/happyTours/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const result = await happyToursCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    //countries
+    app.get("/countries", async (req, res) => {
+      const cursor = countryCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    //search by country name
+    app.get("/happyTours/:name", async (req, res) => {
+      const country = req.params.name;
+      console.log(req);
+      const query = { countryName: country };
+      const result = await happyToursCollection.find(query).toArray();
+      res.send(result);
+      console.log(result, query, country);
+    });
+
+    app.get("/countries/:name", async (req, res) => {
+      const country = req.params.name;
+      console.log(req);
+      const query = { countryName: country };
+      const result = await happyToursCollection.find(query).toArray();
+      res.send(result);
+      console.log(result, query, country);
+    });
 
     // Send a ping to confirm a successful connection
-        //should comment below line
+    //should comment below line
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
